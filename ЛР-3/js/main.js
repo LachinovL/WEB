f876582521845953d430ab6ec36ad4d9f48bd646
LachinovL <57621591+LachinovL@users.noreply.github.com>
2021-05-28 01:47:08 +0300

    Add files via upload
    lr-3 new

--- ЛР-3/js/main.js
+++ ЛР-3/js/main.js
@@ -8,7 +8,12 @@ $(document).ready(function(){
 	$(".button-primary").on("click", function(){
 		email = $ ("#email")[0].value;
 		gender = $("#gender")[0].value; 
+		if(email.length > 0){
 		input = $("<tr><td>" + email + "</td><td>" + gender + "</td><tr>");
 		$("tbody").append(input);
+		} else {
+			alert("Заполните поле e-mail");
+		} 
+		
 	})
 });
